7539b0d96adf82851e887e42861b9bd096481f87
chimaobisvgj <igwilochimaobi139@gmail.com>
2025-09-16 12:53:00 +0200

    initial comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(a() + " " + b());
     }
+
+    public static String a() {return "hello";}
+
+    public static String b() {return "world";}
 }
